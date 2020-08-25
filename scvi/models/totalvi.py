79aaf20149dba5e555ae047fea862f23dc638ceb
adamgayoso <adamgayoso@users.noreply.github.com>
2020-08-25 12:19:18 -0700

    use masked scatter for bg kl in imputation mode

--- scvi/models/totalvi.py
+++ scvi/models/totalvi.py
@@ -513,9 +513,14 @@ class TOTALVI(nn.Module):
                 Normal(py_["back_alpha"], py_["back_beta"]), self.back_mean_prior
             )
             if pro_batch_mask_minibatch is not None:
-                kl_div_back_pro = (pro_batch_mask_minibatch * kl_div_back_pro_full).sum(
-                    dim=1
+                # kl_div_back_pro = (pro_batch_mask_minibatch * kl_div_back_pro_full).sum(
+                #     dim=1
+                # )
+                temp_kl_div_back_pro = torch.zeros_like(kl_div_back_pro_full)
+                temp_kl_div_back_pro.masked_scatter_(
+                    pro_batch_mask_minibatch.bool(), kl_div_back_pro_full
                 )
+                kl_div_back_pro = temp_kl_div_back_pro.sum(dim=1)
             else:
                 kl_div_back_pro = kl_div_back_pro_full.sum(dim=1)
         else:
