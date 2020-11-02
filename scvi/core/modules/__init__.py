21917cd2736f82ea9b839876515467c79cf6e608
galen xing <gx2113@columbia.edu>
2020-11-01 18:34:19 -0800

    add SPLITVAE to init

--- scvi/core/modules/__init__.py
+++ scvi/core/modules/__init__.py
@@ -2,7 +2,17 @@ from .autozivae import AutoZIVAE
 from .classifier import Classifier
 from .jvae import JVAE
 from .scanvae import SCANVAE
+from .splitvae import SPLITVAE
 from .totalvae import TOTALVAE
 from .vae import LDVAE, VAE
 
-__all__ = ["VAE", "LDVAE", "TOTALVAE", "AutoZIVAE", "SCANVAE", "Classifier", "JVAE"]
+__all__ = [
+    "VAE",
+    "LDVAE",
+    "TOTALVAE",
+    "AutoZIVAE",
+    "SCANVAE",
+    "SPLITVAE",
+    "Classifier",
+    "JVAE",
+]
