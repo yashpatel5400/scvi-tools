3c63cd48aa89717f04f89895c9ff0e547fc1351b
Khalil Ouardini <khalil.ouardini@student.ecp.fr>
2021-03-08 23:48:21 -0800

    Unit testing

--- tests/inference/test_message_passing.py
+++ tests/inference/test_message_passing.py
@@ -15,11 +15,24 @@ class TestMessagePassing(TestCase):
     def test_mp_inference(self):
         #tree_name = "../data/lg7_tree_hybrid_priors.alleleThresh.processed.txt"
         tree_name = "../data/tree_test.txt"
+        #tree_name = "../data/richard_tree.nw"
+
         with open(tree_name, "r") as myfile:
             tree_string = myfile.readlines()
 
         tree = Tree(tree_string[0], 1)
         leaves = tree.get_leaves()
+
+        #branch_lengths = {}
+        #for i, n in enumerate(tree.traverse('levelorder')):
+            # n.add_features(index=i)
+            #n.name = str(i)
+            #if n.dist == 0:
+                #branch_lengths[n.name] = 1.0 #eps
+            #else:
+                #branch_lengths[n.name] = 1.0 #n.dist
+        #branch_lengths['prior_root'] = 1.0
+        var = 1.0
 
         # dimension of latent space
         d = 2
@@ -40,7 +53,7 @@ class TestMessagePassing(TestCase):
         gene_dataset.initialize_cell_attribute('barcodes', barcodes)
 
         #create tree dataset
-        cas_dataset = TreeDataset(gene_dataset, tree_name=tree_name)
+        cas_dataset = TreeDataset(gene_dataset, tree=tree_name)
 
         # No batches beacause of the message passing
         use_batches = False
@@ -48,7 +61,9 @@ class TestMessagePassing(TestCase):
         vae = TreeVAE(cas_dataset.nb_genes,
                       tree=cas_dataset.tree,
                       n_batch=cas_dataset.n_batches * use_batches,
-                      n_latent=d)
+                      n_latent=d,
+                      prior_t=var
+                    )
 
         evidence_leaves = np.hstack([np.array([1.0] * d)] * len(leaves))
         evidence = torch.from_numpy(np.array([np.array([1.0] * d)] * len(leaves))).type(torch.DoubleTensor)
@@ -70,7 +85,9 @@ class TestMessagePassing(TestCase):
         print("Test 1: Message passing output O(nd): ", mp_lik)
 
         # likelihood via  covariance matrix Marginalization + inversion
-        leaves_covariance, full_covariance = precision_matrix(tree_name, d, 1)
+        leaves_covariance, full_covariance = precision_matrix(tree=tree_name,
+                                                              d=d,
+                                                              branch_length=var)
         leaves_mean = np.array([0] * len(leaves) * d)
         pdf_likelihood = multivariate_normal.logpdf(evidence_leaves,
                                                     leaves_mean,
@@ -81,58 +98,66 @@ class TestMessagePassing(TestCase):
 
         #####################
         print("")
-        print("Test 2: Message Passing Posterior Predictive Density at internal node")
+        print("Test 2: Message Passing Posterior Predictive Density at internal nodes ")
+        for n in vae.tree.traverse():
 
-        #query_node = '0|0|0|0|9|0|3|7|0|-|-|-|16|0|0|0|2|0|0|2|0|0|0|0|8|0|2|0|0|-|-|2|0|0|0|0|0|-|-|-|0|0|0|2|2|2|0|0' # --> internal node
-        query_node = "0|0|0|0|5|10|0|0|0|2|0|0|0|0|0|0|0|0|2|0|0|0|0|0|0|0|0|0|0"
+            if n.is_leaf() or n.is_root():
+                continue
+            if n.name == 'prior_root':
+                continue
+            #query_node = '0|0|0|0|9|0|3|7|0|-|-|-|16|0|0|0|2|0|0|2|0|0|0|0|8|0|2|0|0|-|-|2|0|0|0|0|0|-|-|-|0|0|0|2|2|2|0|0' # --> internal node
+            #query_node = "0|0|0|0|5|10|0|0|0|2|0|0|0|0|0|0|0|0|2|0|0|0|0|0|0|0|0|0|0"
 
-        # evidence
-        evidence_leaves = np.hstack([np.array([1.0] * d)] * (len(leaves)))
+            query_node = n.name
+            print("Query node:", query_node)
 
-        # MP call from the query node
+            # evidence
+            evidence_leaves = np.hstack([np.array([1.0] * d)] * (len(leaves)))
 
-        # Gaussian conditioning formula
-        to_delete_idx_ii = [i for i in list(range(N)) if (i not in leaves_index)]
-        to_delete_idx_ll = [i for i in list(range(N)) if (i in leaves_index)]
+            # MP call from the query node
 
-        # partition index
-        I = [idx for idx in list(range(N)) if idx not in to_delete_idx_ii]
-        L = [idx for idx in list(range(N)) if idx not in to_delete_idx_ll]
+            # Gaussian conditioning formula
+            to_delete_idx_ii = [i for i in list(range(N)) if (i not in leaves_index)]
+            to_delete_idx_ll = [i for i in list(range(N)) if (i in leaves_index)]
 
-        # covariance marginalization
-        cov_ii = marginalize_covariance(full_covariance, [to_delete_idx_ii], d)
-        cov_ll = marginalize_covariance(full_covariance, [to_delete_idx_ll], d)
-        cov_il = marginalize_covariance(full_covariance, [to_delete_idx_ii, to_delete_idx_ll], d)
-        cov_li = np.copy(cov_il.T)
+            # partition index
+            I = [idx for idx in list(range(N)) if idx not in to_delete_idx_ii]
+            L = [idx for idx in list(range(N)) if idx not in to_delete_idx_ll]
 
-        internal_post_mean_transform = np.dot(cov_li, np.linalg.inv(cov_ii))
-        internal_post_covar = cov_ll - np.dot(np.dot(cov_li, np.linalg.inv(cov_ii)), cov_il)
+            # covariance marginalization
+            cov_ii = marginalize_covariance(full_covariance, [to_delete_idx_ii], d)
+            cov_ll = marginalize_covariance(full_covariance, [to_delete_idx_ll], d)
+            cov_il = marginalize_covariance(full_covariance, [to_delete_idx_ii, to_delete_idx_ll], d)
+            cov_li = np.copy(cov_il.T)
 
-        # Message Passing
-        vae.initialize_visit()
-        vae.initialize_messages(
-            evidence,
-            cas_dataset.barcodes,
-            d
-        )
-        vae.perform_message_passing((vae.tree & query_node), d, True)
-        query_idx = (tree & query_node).index
-        query_idx = L.index(query_idx)
+            internal_post_mean_transform = np.dot(cov_li, np.linalg.inv(cov_ii))
+            internal_post_covar = cov_ll - np.dot(np.dot(cov_li, np.linalg.inv(cov_ii)), cov_il)
 
-        post_mean = np.dot(internal_post_mean_transform, np.hstack(evidence_leaves))[query_idx * d: query_idx * d + 2]
-        print("Test2: Gaussian conditioning formula O(n^3d^3): ",
-              post_mean,
-              internal_post_covar[query_idx * d, query_idx * d])
+            # Message Passing
+            vae.initialize_visit()
+            vae.initialize_messages(
+                evidence,
+                cas_dataset.barcodes,
+                d
+            )
+            vae.perform_message_passing((vae.tree & query_node), d, True)
+            query_idx = (tree & query_node).index
+            query_idx = L.index(query_idx)
 
-        print("Test2: Message passing output O(nd): ",
-              (vae.tree & query_node).mu, (vae.tree & query_node).nu
-              )
+            post_mean = np.dot(internal_post_mean_transform, np.hstack(evidence_leaves))[query_idx * d: query_idx * d + 2]
+            print("Test2: Gaussian conditioning formula O(n^3d^3): ",
+                  post_mean,
+                  internal_post_covar[query_idx * d, query_idx * d])
 
-        self.assertTrue((np.abs((vae.tree & query_node).nu - internal_post_covar[query_idx * d, query_idx * d])) < 1e-8)
-        self.assertTrue((np.abs((vae.tree & query_node).mu[0] - post_mean[0])) < 1e-8)
-        self.assertTrue((np.abs((vae.tree & query_node).mu[1] - post_mean[1])) < 1e-8)
+            print("Test2: Message passing output O(nd): ",
+                  (vae.tree & query_node).mu, (vae.tree & query_node).nu
+                  )
 
-        print("")
+            self.assertTrue((np.abs((vae.tree & query_node).nu - internal_post_covar[query_idx * d, query_idx * d])) < 1e-8)
+            self.assertTrue((np.abs((vae.tree & query_node).mu[0] - post_mean[0])) < 1e-8)
+            self.assertTrue((np.abs((vae.tree & query_node).mu[1] - post_mean[1])) < 1e-8)
+
+            print("")
 
 if __name__ == '__main__':
     unittest.main()
