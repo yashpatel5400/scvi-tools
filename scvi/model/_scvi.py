22a97401e1edbf3f282689d9b868e27bf66b47d7
adamgayoso <adamgayoso@users.noreply.github.com>
2021-11-20 11:02:25 -0800

    add adversarial training option to scvi

--- scvi/model/_scvi.py
+++ scvi/model/_scvi.py
@@ -1,23 +1,23 @@
 import logging
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from anndata import AnnData
+import numpy as np
 
 from scvi._compat import Literal
 from scvi.data._anndata import _setup_anndata
 from scvi.model._utils import _init_library_size
-from scvi.model.base import UnsupervisedTrainingMixin
-from scvi.module import VAE
+from scvi.module import VAE, Classifier
 from scvi.utils import setup_anndata_dsp
+from scvi.dataloaders import DataSplitter
+from scvi.train import AdversarialTrainingPlan, TrainRunner
 
 from .base import ArchesMixin, BaseModelClass, RNASeqMixin, VAEMixin
 
 logger = logging.getLogger(__name__)
 
 
-class SCVI(
-    RNASeqMixin, VAEMixin, ArchesMixin, UnsupervisedTrainingMixin, BaseModelClass
-):
+class SCVI(RNASeqMixin, VAEMixin, ArchesMixin, BaseModelClass):
     """
     single-cell Variational Inference [Lopez18]_.
 
@@ -163,3 +163,89 @@ class SCVI(
             continuous_covariate_keys=continuous_covariate_keys,
             copy=copy,
         )
+
+    def train(
+        self,
+        max_epochs: Optional[int] = None,
+        use_gpu: Optional[Union[str, int, bool]] = None,
+        train_size: float = 0.9,
+        validation_size: Optional[float] = None,
+        batch_size: int = 128,
+        early_stopping: bool = False,
+        adversarial_classifier: Union[bool, Classifier] = False,
+        scale_adversarial_loss: Union[float, Literal["auto"]] = "auto",
+        plan_kwargs: Optional[dict] = None,
+        **trainer_kwargs,
+    ):
+        """
+        Train the model.
+
+        Parameters
+        ----------
+        max_epochs
+            Number of passes through the dataset. If `None`, defaults to
+            `np.min([round((20000 / n_cells) * 400), 400])`
+        use_gpu
+            Use default GPU if available (if None or True), or index of GPU to use (if int),
+            or name of GPU (if str, e.g., `'cuda:0'`), or use CPU (if False).
+        train_size
+            Size of training set in the range [0.0, 1.0].
+        validation_size
+            Size of the test set. If `None`, defaults to 1 - `train_size`. If
+            `train_size + validation_size < 1`, the remaining cells belong to a test set.
+        batch_size
+            Minibatch size to use during training.
+        early_stopping
+            Perform early stopping. Additional arguments can be passed in `**kwargs`.
+            See :class:`~scvi.train.Trainer` for further options.
+        adversarial_classifier
+            Whether to use adversarial classifier in the latent space to improve batch
+            mixing. This will only consider the metadata registered using `batch_key`.
+        scale_adversarial_loss
+            Scaling factor on the adversarial components of the loss.
+            By default, adversarial loss is scaled from 1 to 0 following opposite of
+            kl warmup. A higher value will induce more latent space mixing.
+        plan_kwargs
+            Keyword args for :class:`~scvi.train.TrainingPlan`. Keyword arguments passed to
+            `train()` will overwrite values present in `plan_kwargs`, when appropriate.
+        **trainer_kwargs
+            Other keyword args for :class:`~scvi.train.Trainer`.
+        """
+        if max_epochs is None:
+            n_cells = self.adata.n_obs
+            max_epochs = np.min([round((20000 / n_cells) * 400), 400])
+
+        plan_kwargs = plan_kwargs if isinstance(plan_kwargs, dict) else dict()
+
+        data_splitter = DataSplitter(
+            self.adata,
+            train_size=train_size,
+            validation_size=validation_size,
+            batch_size=batch_size,
+            use_gpu=use_gpu,
+        )
+
+        update_dict = {
+            "adversarial_classifier": adversarial_classifier,
+            "scale_adversarial_loss": scale_adversarial_loss,
+        }
+        if plan_kwargs is not None:
+            plan_kwargs.update(update_dict)
+        else:
+            plan_kwargs = update_dict
+
+        training_plan = AdversarialTrainingPlan(self.module, **plan_kwargs)
+
+        es = "early_stopping"
+        trainer_kwargs[es] = (
+            early_stopping if es not in trainer_kwargs.keys() else trainer_kwargs[es]
+        )
+        runner = TrainRunner(
+            self,
+            training_plan=training_plan,
+            data_splitter=data_splitter,
+            max_epochs=max_epochs,
+            use_gpu=use_gpu,
+            **trainer_kwargs,
+        )
+        return runner()
